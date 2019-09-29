70eef14489c4e324d00333f8735f04c5c7fe198f
Iqbal Syamil Ayasy <iqbalsyamilayas@gmail.com>
2019-09-29 18:36:35 +0700

    add animeLink data

--- src/store/listAnime.js
+++ src/store/listAnime.js
@@ -2,17 +2,20 @@ export default [
     {
         animeEmoji:" 🏀 🏆 ⛹🏻‍♂️",
         animeName:"Kuroko's Basketball",
-        animeYear:2012
+        animeYear:2012,
+        animeLink:"https://myanimelist.net/anime/11771/Kuroko_no_Basket"
     },
     {
         animeEmoji:" 👦🏻👩🏼🎻🎹😭" ,
         animeName:"Shigatsu wa kimi no uso",
-        animeYear:2014
+        animeYear:2014,
+        animeLink:"https://myanimelist.net/anime/23273/Shigatsu_wa_Kimi_no_Uso"
     },
     {
         animeEmoji:" ⚔️⚡️🙍🏻‍♂️🙍🏼‍♀️🧚🏻‍♂️" ,
         animeName:"Sword Art Online",
-        animeYear:2009
+        animeYear:2012,
+        animeLink:"https://myanimelist.net/anime/11757/Sword_Art_Online"
     }
 
 ]
