bb0ea5a6c5126902c2a4c5129e9abb1c73fd18f2
Mitchell Reiff <mapr323@gmail.com>
2019-10-01 16:24:17 -0400

    Added Overlord

--- src/store/listAnime.js
+++ src/store/listAnime.js
@@ -23,6 +23,11 @@ export default [
         animeYear:2012,
         animeLink:"https://myanimelist.net/anime/11771/Kuroko_no_Basket"
     },
+    {   animeEmoji:"💀 🏰 🐹",
+        animeName:"Overlord",
+        animeYear:2015,
+        animeLink:"https://myanimelist.net/anime/29803/Overlord"
+    },
     {   animeEmoji:"🤖🙎‍♀️😭🙍🏻‍♂️",
         animeName:"Plastic Memories",
         animeYear:2015,
