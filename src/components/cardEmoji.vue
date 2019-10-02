ee97036282ddb55db85ec2fee89a8832d044d2dc
frankwaldal <frank.sondre.waldal@entur.org>
2019-10-02 03:59:18 +0200

    made whole emoji-card clickable, both as closed and open, fixed padding

--- src/components/cardEmoji.vue
+++ src/components/cardEmoji.vue
@@ -2,10 +2,10 @@
     <div class="card">
         <div class="card-body text-center h2" data-toggle="collapse" :href="`#desc-${animeId}`" aria-expanded="false">
             {{animeEmoji}}
-        </div>
-        <div class="card-body text-center collapse" v-bind:id="`desc-${animeId}`">
-            <div class="card-title h5">
-                {{animeName}} ({{animeYear}})
+            <div class="card-hidden text-center collapse" v-bind:id="`desc-${animeId}`">
+                <div class="card-title h5">
+                    {{animeName}} ({{animeYear}})
+                </div>
             </div>
         </div>
     </div>
@@ -25,5 +25,15 @@ export default {
 <style scoped>
 .card{
     user-select: none;
+}
+.card-hidden {
+    padding-top: 1.5rem;
+}
+.card-title {
+    margin-bottom: 0;
+}
+.card-body {
+margin-bottom: 0;
+padding-bottom: 1.5rem;
 }
 </style>
