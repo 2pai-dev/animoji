3d578bbced5125d099066d5eda52a4375df64209
Pongsakorn Ajcahriyasakchai <kigdomheart.june@gmail.com>
2019-10-18 09:59:30 +0700

    Fixed missing , bug. Add , to line 744

--- src/store/listAnime.js
+++ src/store/listAnime.js
@@ -741,7 +741,7 @@ export default [{
     animeName:'Yahari Ore no Seishun Love Comedy wa Machigatteiru.',
     animeYear:2013,
     animeLink:'https://myanimelist.net/anime/14813/Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru'
-  }
+  },
   {
     animeEmoji:'👦🎮👧👧',
     animeName:'High score girl.',
