90d5a60ce2fbee470acff3b5ef8e86bd7a0e38a2
Putra-febriansyh4646 <anbufebri@gmail.com>
2020-10-01 16:06:33 +0700

    add Quanzhi-Gaoshou

--- src/store/listAnime.js
+++ src/store/listAnime.js
@@ -894,5 +894,13 @@ export default [
     animeName:'Mahouka Koukou no Rettousei',
     animeYear:2014,
     animeLink:'https://myanimelist.net/anime/20785/Mahouka_Koukou_no_Rettousei'
+  },
+  {
+    animeEmoji:'⚔️🎮',
+    animeName:'Quanzhi Gaoshou',
+    animeYear:2019,
+    animeLink:'https://myanimelist.net/anime/33926/Quanzhi_Gaoshou', 
   }
 ];
+
+
