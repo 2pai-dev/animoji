51c4ce54a5d67735250e088c813bc66e56cda075
Mohamad Radisha <pr0ph0z23@gmail.com>
2020-10-21 10:42:26 +0700

    Update Pull Request Template (#303)
    * Remove unused `filename` property
    
    * Update Pull Request template

--- src/datasource/index.js
+++ src/datasource/index.js
@@ -1,15 +1,13 @@
 function requireAll(r) {
     return r.keys();
 }
-  
-  
+
 const anime = requireAll(require.context('./', true, /\.json$/));
 function getListAnime() {
     const items = [];
     anime.forEach(i => {
         const r = i.replace('./', '').replace('.json', '');
         const item = require(`./${r}`);
-        item.filename = r;
         items.push(item);
     });
     return items;
