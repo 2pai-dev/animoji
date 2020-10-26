b632cb0db1b4230d1bb45d0f6a9699c1515eebe1
Mohamad Radisha <pr0ph0z23@gmail.com>
2020-10-26 18:48:42 +0700

    CI Pipeline (#312)
    * Remove unused `filename` property
    
    * Update Pull Request template
    
    * Add support to be able take argument in validating data source
    
    * Rename data source file name to kebab-case
    
    * Create CI pipeline
    
    * Update kebab-case rule in pull request template

--- src/datasource/validate.js
+++ src/datasource/validate.js
@@ -1,4 +1,5 @@
-const glob = require('glob')
+const glob = require('glob');
+const path = require('path');
 
 function countEmoji(emojis) {
     return emojis.map(emoji => {
@@ -20,8 +21,21 @@ function countEmoji(emojis) {
 }
 
 glob(`${__dirname}/*.json`, {}, (err, files) => {
-    files.forEach(file => {
-        const jsonFile = require(file)
-        if (countEmoji(jsonFile.animeEmoji.split(' ')) > 8) process.exit(1)
-    })
+    if (process.argv.slice(2)[0] === 'total-emoji') {
+        files.forEach(file => {
+            const jsonFile = require(file)
+            if (countEmoji(jsonFile.animeEmoji.split(' ')) > 8) {
+                process.stdout.write(`${jsonFile.animeName} has emoji more than 8.\n`);
+                process.exit(1)
+            }
+        })
+    } else if (process.argv.slice(2)[0] === 'file-name') {
+        files.forEach(file => {
+            const fileName = path.parse(file).name
+            if (!new RegExp(/^([a-z0-9][a-z0-9]*)(-[a-z0-9]+)*$/).test(fileName)) {
+                process.stdout.write(`${fileName} is not in the right pattern. Please use kebab-case.\n`);
+                process.exit(1);
+            }
+        });
+    }
 })
