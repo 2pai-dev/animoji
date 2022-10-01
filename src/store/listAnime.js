63a5e9c306da062489474a16857a2b9dcd4d5baf
Fadhlan Ar-rizal <fadhlanarrizal@gmail.com>
2022-10-01 13:39:36 +0700

    add fairy tail & hunter x hunter anime

---
+++ src/store/listAnime.js
@@ -0,0 +1,15 @@
+export default
+[
+    {
+        animeEmoji:"🧚‍♂️ 😻 🐉 🪄",
+        animeName:"Fairy Tail",
+        animeYear:"2009",
+        animeLink:"https://myanimelist.net/anime/6702/Fairy_Tail?q=fair&cat=anime"
+    },
+    {
+        animeEmoji:"⚔️ ☀️ 🐺 🐗",
+        animeName:"Hunter x Hunter",
+        animeYear:"1999",
+        animeLink:"https://myanimelist.net/anime/136/Hunter_x_Hunter?q=hunter&cat=anime"
+    }
+]
